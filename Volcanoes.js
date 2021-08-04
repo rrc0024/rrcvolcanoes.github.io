58410fedfea0ac0599a7428d8141cbac70736f1a
rrc0024 <63365748+rrc0024@users.noreply.github.com>
2021-08-03 23:00:21 -0400

    First Correct Quiz Question Changes

--- Volcanoes.js
+++ Volcanoes.js
@@ -2,7 +2,7 @@
 var correctList = document.getElementsByClassName("correct");
 var incorrectList = document.getElementsByClassName("incorrect");
 var firstCorrect = document.getElementById("onecorrect");
-var firstCorrectMsg = document.getElementById("
+var firstCorrectMsg = document.getElementById("firstCorrectMsg");
 
 var onButtonClick = function(){
   firstCorrect.style.color = "green";
@@ -10,6 +10,6 @@ var onButtonClick = function(){
 correctList.addEventListener("click", onButtonClick);
 
 if firstCorrect.style.color = "green" {
-  
+  firstCorrectMsg.style.visibility = "hidden";
 }
   
