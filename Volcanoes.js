e400be416d7da45e979aad2bef5fdc4d29bcc902
rrc0024 <63365748+rrc0024@users.noreply.github.com>
2021-08-03 22:57:10 -0400

    Update Volcanoes.js

--- Volcanoes.js
+++ Volcanoes.js
@@ -2,8 +2,14 @@
 var correctList = document.getElementsByClassName("correct");
 var incorrectList = document.getElementsByClassName("incorrect");
 var firstCorrect = document.getElementById("onecorrect");
+var firstCorrectMsg = document.getElementById("
 
 var onButtonClick = function(){
   firstCorrect.style.color = "green";
 };
 correctList.addEventListener("click", onButtonClick);
+
+if firstCorrect.style.color = "green" {
+  
+}
+  
